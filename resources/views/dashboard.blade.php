c58ad78469873c153c357d354b554890fe989826
Lucas Fernando <lucasf.geron@icloud.com>
2021-04-22 13:48:05 -0300

    first models

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-
             <div class="flex items-center justify-center">
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
 
                     <!-- 1 card -->
-                    <div class="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                    <div class=" bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
                         <a href="{{ route('categorias') }}" class="text-xl font-semibold my-2 hover:text-blue-500 ">Categorias</a>
                         <div class="flex space-x-2 text-gray-400 text-sm my-3">
                             <p>Total: 2 / 2</p>
@@ -46,9 +45,6 @@
                             <p>Total: 17</p>
                         </div>
                     </div>
-
-                    
-
 
 
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,3 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/categorias', function () {
-    return view('dashboard');
-})->name('categorias');
